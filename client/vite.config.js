d01c65486a94b14c41efda572cbfbea272172871
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-09-05 17:23:23 -0700

    server changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      "/graph1": {
+      "/graphql": {
         target: "http://localhost:3001",
         secure: false,
         changeOrigin: true,
